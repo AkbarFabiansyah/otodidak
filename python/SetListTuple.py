42fe354144e4f6f618dba54d883178eb63a93279
AkbarFabiansyah <realakbarofc@gmail.com>
2026-03-08 23:07:27 +0700

    number 5

---
+++ python/SetListTuple.py
@@ -0,0 +1,17 @@
+#Set (Himpunan)
+HimpunanNomor = {1, 1, 2, 3, 3, 3}
+
+HimpunanBuah = {'🍎', '🍓', '🍐', '🍎', '🍎', '🍓'}
+
+print(HimpunanNomor)
+print(HimpunanBuah)
+
+#list
+ListMasakan =  ['NasiPadang', 'nasigoreng', 'Nasi Kuning']
+print (ListMasakan)
+#Tuple
+Motor = ('Honda','Yamaha', 'suzuki')
+print (Motor[0])
+print (Motor[2])
+print (ListMasakan[2])
+
