60a98a56cb0a3f6bc0ad3173996bc11bdb4fe98c
AkbarFabiansyah <realakbarofc@gmail.com>
2026-03-08 21:43:36 +0700

    nomor 4

---
+++ python/conditionals.py
@@ -0,0 +1,20 @@
+#introduction to The if statement 
+
+x = 0
+y = 5
+if x < y:                            
+   print('yes')
+if y < x:                            
+  print('yes')
+if x:                                
+  print('yes')
+if y:                                
+  print('yes')
+if x or y:                           
+   print('yes')
+if x and y:                          
+    print('yes')
+if 'aul' in 'grault':                
+   print('yes')
+if 'quux' in ['foo', 'bar', 'baz']:  
+   print('yes')
